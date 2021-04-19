aeaafd08853eb80ecfcf07f50939b634fc64f75d
hkfrtyseven <facque3@gmail.com>
2021-04-19 12:53:34 -0700

    Matt's version of camera movement

---
+++ Bug Zapper/Assets/MFTESTSCRIPT/movement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public Vector3 position;
+    public GameObject gamePiece;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            position.z += 25 * Time.deltaTime;
+        if (Input.GetKey(KeyCode.S))
+            position.z -= 25 * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+            position.x -= 25 * Time.deltaTime;
+        if (Input.GetKey(KeyCode.D))
+            position.x += 25 * Time.deltaTime;
+
+        
+    }
+}
---
+++ Bug Zapper/Assets/MFTESTSCRIPT/rbmovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rbmovement : MonoBehaviour
+{
+    public GameObject gameObject;
+    public Vector3 position;
+    public float speed = 150.0f;
+    public Rigidbody rb;
+    public Vector3 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));    
+    }
+
+    private void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+
+    void moveCharacter(Vector2 direction)
+    {
+        rb.MovePosition(transform.position + movement * speed * Time.deltaTime);
+    }
+}
